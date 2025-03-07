363c7f118e7b204c2c96311444722d763afd19df
AlexanderBadenhorst <alexander.baden15.ab@gmail.com>
2025-03-07 11:48:43 +0200

    first commit

--- index.js
+++ index.js
@@ -13,11 +13,18 @@ const mongoose = require('mongoose')
 const User = require('./models/user.model')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect('mongodb://localhost:27017/final-mern-app')
+mongoose.connect(process.env.MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+})
+	.then(() => console.log('Connected to MongoDB'))
+	.catch((err) => console.error('Could not connect to MongoDB', err));
 
 app.post('/api/register', async (req, res) => {
 	console.log(req.body)
